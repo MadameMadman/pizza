108bb6782ce49e0bf539a6b43a542c6af6a3e6f4
MadameMadman <nico.daunt@gmail.com>
2017-12-11 12:37:38 -0800

    finish the prototype and append descriptions to order

---
+++ README.md
@@ -0,0 +1,35 @@
+# _Nico's Pizza Order Form_
+
+[CLICK HERE TO VIEW IN BROWSER](https://madamemadman.github.io/pizza/)
+
+#### _Object-Oriented Javascript Code Review for Epicodus_
+
+#### By _**Nico Daunt**_
+
+## Description
+
+A bootstrapped Pizza Order Form made with Javascript Objects.
+_
+
+## Setup/Installation Requirement
+
+* git clone https://github.com/MadameMadman/pizza.git
+* run _index.html_
+
+
+## Support and contact details
+
+_nico.daunt@gmail.com_
+
+## Technologies Used
+
+* html
+* css
+* jquery 3.2.1
+* bootstrap 3.3.7
+
+### License
+
+*Open Source*
+
+Copyright (c) 2017 **_Nico Daunt, Epicodus_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,50 @@
-function Pizza(size,cheese) {
+var topping = [];
+
+function Pizza(size,topping) {
   this.size = size;
-  this.cheeze = cheese;
-  this.baseCost = 0;
+  this.topping = topping;
+  this.price;
+};
+
+Pizza.prototype.makeCost = function() {
+  this.price = 12;
+    if (this.size === 'small') {
+      this.price -= 3;
+    } else if (this.size === 'medium') {
+      this.price += 3;
+    } else if (this.size === 'large') {
+      this.price += 10;
+    }
+    for (var i = 0; i < this.topping.length; i++) {
+    this.price += 2;
+  }
+
 }
 
-Pizza.prototype.makeCost = function(size, cheesee) {
-  this.cost = parseInt(this.size + this.cheese);
-
-};
+Pizza.prototype.shortDescription = function() {
+  return this.size + " " + this.topping + " pizza: $" + this.price;
+}
 
 
 //front-end section
 
 $(document).ready (function(){
 
-	$("#pizzaSubmit").click(function(){
+	$("form#pizzaSubmit").submit(function(event){
     event.preventDefault();
-  	var size = $("input[name=size]:checked").val()
-    var cheese = $("input:checkbox[name=cheese]:checked").val()
-    console.log(size, cheese);
-    var myOrder = new Pizza(size, cheese);
-  	$("#result").append("$" + parseInt(myOrder.makeCost()) + " ");
+  	var size = $("input[name=size]:checked").val();
+    if (!size) {
+      alert("please enter a size");
+    return false;
+  };
+    var topping = $("input[name='topping']:checked").map(function() {
+        return this.value;
+      }).get();
+    var newPizza = new Pizza(size, topping);
+    newPizza.makeCost();
+  	$("ul#result").append("<li> " + newPizza.shortDescription() + "</li>");
+
   });
+
+
 });
