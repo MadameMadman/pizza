22bb61c46fa55e91dc6a943350fa79690927811d
MadameMadman <nico.daunt@gmail.com>
2017-12-11 01:42:12 -0800

    make a majority of the scripting neccessary for the order form, but still missing a working submit feature, console log is showing defined variables, but the front-end still seems off

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
-//business logic
+function Pizza(size,cheese) {
+  this.size = size;
+  this.cheeze = cheese;
+  this.baseCost = 0;
+}
 
-//front-end logic
-$(document).ready(function(){
+Pizza.prototype.makeCost = function(size, cheesee) {
+  this.cost = parseInt(this.size + this.cheese);
+
+};
 
 
+//front-end section
+
+$(document).ready (function(){
+
+	$("#pizzaSubmit").click(function(){
+    event.preventDefault();
+  	var size = $("input[name=size]:checked").val()
+    var cheese = $("input:checkbox[name=cheese]:checked").val()
+    console.log(size, cheese);
+    var myOrder = new Pizza(size, cheese);
+  	$("#result").append("$" + parseInt(myOrder.makeCost()) + " ");
+  });
 });
